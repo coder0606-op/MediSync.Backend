59972fa872de45d182b6d283726ecad4749f730f
coder0606-op <iamayush616@gmail.com>
2025-05-07 01:02:46 +0530

    Major

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
 import userRouter from "./routes/userRoute.js";
-
+import paymentRoutes from './routes/paymentRoute.js';
+import bodyParser from 'body-parser';
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
@@ -23,7 +24,7 @@ app.use(cors({
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/user", userRouter);
-
+app.use("/api", paymentRoutes);
 app.get("/", (req, res) => {
   res.send("Hello from the backend!");
 });
